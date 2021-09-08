73f570a1c2e1991c2aea8a524fd90963214c6bcf
fullstackgjj <fullstackgjj@gmail.com>
2021-09-07 21:35:37 -0400

    implemented domain

---
+++ src/CitiAccountTransaction/DomainModels.hs
@@ -0,0 +1,16 @@
+module CitiAccountTransaction.DomainModels where
+
+type Date = String
+
+type Description = String
+
+type Debit = Float
+
+type Credit = Float
+
+data BankAccountTransaction = BankAccountTransaction {
+    date::Date,
+    description::Description,
+    debit::Debit,
+    credit::Credit
+} deriving (Eq, Show)
